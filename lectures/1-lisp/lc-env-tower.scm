21ffd36e9fd598cfbe895f655f4c5427cad5b0e6
Nada Amin <namin@seas.harvard.edu>
2025-06-14 15:06:47 -0400

    scaffolding from lc-tower.scm

--- lectures/1-lisp/lc-env-tower.scm
+++ lectures/1-lisp/lc-env-tower.scm
@@ -7,6 +7,36 @@
 
 (define empty-env (lambda (y) (error 'env "unbound variable")))
 
+(define (make-global-env) empty-env)
+
+(define (env-extend env params args)
+  (if (null? params)
+      env
+      (let ((x (car params))
+            (a (car args)))
+        (env-extend (lambda (y) (if (eq? x y) a (env y))) (cdr params) (cdr args)))))
+
+(define make-meta-cont-level
+  (lambda (level)
+    (let ((upper-env (make-global-env)))
+      (cons upper-env
+            (lambda (val mc)
+              (display (format "Returned to level ~a with: " level))
+              (display val)
+              (newline)
+              val)))))
+
+(define get-meta-cont
+  (lambda (level)
+    (cons (make-meta-cont-level level)
+          (lambda () (get-meta-cont (+ level 1))))))
+
+(define meta-cont-force
+  (lambda (mc)
+    (if (procedure? (cdr mc))
+        (cons (car mc) ((cdr mc)))
+        mc)))
+
 (define lc
   (lambda (exp env)
     (cond
@@ -33,3 +63,13 @@
       ((lc (car exp) env) (lc (cadr exp) env))))))
 
 (lc-tests lc empty-env)
+
+(define (lc-reflective-tests lc empty-env)
+  (eg (lc '((mu (e r) (meaning 1 r))) empty-env) 1)
+  (eg (lc '((mu (e r) (meaning (car e) r)) 1) empty-env) 1)
+  (eg (lc '((mu (e r) (meaning (car e) r)) (sub1 2)) empty-env) 1)
+  (eg (lc '((mu (e1 r1) ((delta (e2 r2) (meaning 1 r2))))) empty-env) 1)
+)
+
+;(lc-reflective-tests lc empty-env)
+
