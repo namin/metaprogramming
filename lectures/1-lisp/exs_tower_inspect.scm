3418daefba37dd6f45ce7c134b077922aeebeabe
Nada Amin <namin@seas.harvard.edu>
2025-06-23 23:25:48 -0400

    cleanup

--- lectures/1-lisp/exs_tower_inspect.scm
+++ lectures/1-lisp/exs_tower_inspect.scm
@@ -1,17 +1,23 @@
-(define f ((lambda (x) (lambda (y) (+ x y))) 3))
-(f 7)
-((delta (e r k) (begin (set-cdr! (car (car (cadr (r 'f)))) 5) (k 0))))
-
 (define foo (lambda (f) (lambda (x) (lambda () (f (+ x 1))))))
 (define thunk ((foo 2) 3))
 (thunk)
 (old-cont 'ok)
 
-((delta (e r k)
-  (begin
-    (load "tower.scm")
-    (let ((b (find-binding (cadr (r 'thunk)) 'f)))
-      (set-cdr! b (lambda (x) (* 2 x)))
-      (k 0)))))
+(define inspect
+  (lambda (c)
+    ((delta (e r k)
+       (let ((env (cadr (r 'c))))
+         (define loop
+           (lambda ()
+             (display "inspect> ")
+             (let ((exp (read)))
+               (cond
+                 ((and (pair? exp) (eq? (car exp) 'exit))
+                  (meaning (cadr exp) env k))
+                 (else
+                  (meaning exp env (lambda (v) (write v) (newline) (loop))))))))
+         (loop))))))
 
+(inspect thunk)
 
+(thunk)
